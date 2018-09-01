e297f8dc859ba68980bc791eebc757afc0f6808b
rongyu <ronniery@aliyun.com>
2018-09-01 17:33:01 +0800

    add comment

--- spring-data-jpa/src/test/java/com/ronnie/data/ApplicationTests.java
+++ spring-data-jpa/src/test/java/com/ronnie/data/ApplicationTests.java
@@ -9,13 +9,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.*;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
 import java.util.Optional;
-
-import static sun.plugin.util.ProgressMonitor.get;
 
 @Slf4j
 @RunWith(SpringRunner.class)
@@ -28,8 +28,8 @@ public class ApplicationTests {
     MemberOrderDao orderDao;
 
     @Test
-    public void contextLoads() throws InterruptedException {
-       // Member member = memberDao.getOne(1L);
+    public void addTestData() throws InterruptedException {
+        // Member member = memberDao.getOne(1L); bad way
         Optional<Member> optionalMember = memberDao.findById(1L);
         Member member = optionalMember.get();
         for (int i = 0; i < 12; i++) {
@@ -43,18 +43,22 @@ public class ApplicationTests {
     }
 
     @Test
-    public void queryDemo() throws InterruptedException {
+    public void queryDemo() {
+        // add condition
         MemberOrder order = new MemberOrder();
         Member member = new Member();
         member.setUsername("root");
         order.setMember(member);
         Example<MemberOrder> example = Example.of(order);
+        // add order by
         Sort.Order order1 = new Sort.Order(Sort.Direction.DESC, "id");
         Sort.Order order2 = new Sort.Order(Sort.Direction.ASC, "number");
-        Sort sort = Sort.by(order1,order2);
+        Sort sort = Sort.by(order1, order2);
+        // add pageable
         PageRequest pageRequest = PageRequest.of(0, 10, sort);
+        // findAll with page
         Page<MemberOrder> result = orderDao.findAll(example, pageRequest);
-        log.info("result={}",result);
+        log.info("result={}", result);
     }
 
 }
